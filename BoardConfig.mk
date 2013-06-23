94deb9187bfd908a238c6afee301bfeffc57475c
arrrghhh <arrrghhh@xdandroid.com>
2013-06-22 21:13:10 -0600

    Remove copy module lines

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,12 +16,6 @@ BOARD_HAVE_NFC := true
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8960-common
 TARGET_KERNEL_CONFIG := msm8960_mmi_defconfig
 
-# copy all kernel modules under the "kernel/modules" directory to system/lib/modules
-PRODUCT_COPY_FILES += $(shell \
-    find $(LOCAL_PATH)/kernel/modules -name '*.ko' 2> /dev/null \
-    | sed -r 's/^\/?(.*\/)([^/ ]+)$$/\1\2:system\/lib\/modules\/\2/' \
-    | tr '\n' ' ')
-
 BOARD_KERNEL_CMDLINE := console=/dev/null androidboot.hardware=qcom user_debug=31 loglevel=1 msm_rtb.filter=0x3F kgsl.mmutype=gpummu zcache
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
