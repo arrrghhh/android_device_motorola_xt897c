e8a5d093759d1fb679eb4d355e38249893719a94
arrrghhh <arrrghhh@xdandroid.com>
2013-06-26 10:52:50 -0600

    Fixup vendor derp

--- device_xt897c.mk
+++ device_xt897c.mk
@@ -75,5 +75,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/gps.conf:system/etc/gps.conf
 
 $(call inherit-product, device/motorola/msm8960-common/modules/nfc/nfc.mk)
-$(call inherit-product-if-exists, vendor/motorola/xt897/xt897-vendor.mk)
+$(call inherit-product-if-exists, vendor/motorola/msm8960-common/msm8960-common-vendor.mk)
 
