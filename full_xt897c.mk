9e3a4c4fde2b252b8a46b15c659a8b574b4b4584
arrrghhh <arrrghhh@xdandroid.com>
2013-07-03 22:01:09 -0600

    Update for cfx
    Change-Id: I9793c07511361bea75c693df29c7825462bdcfde

--- full_xt897c.mk
+++ full_xt897c.mk
@@ -2,6 +2,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 $(call inherit-product, device/motorola/xt897c/device_xt897c.mk)
 
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+$(call inherit-product, vendor/cfx/config/cdma.mk)
+
+$(call inherit-product, vendor/cfx/config/common_full_phone.mk)
+
+# Copy Bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/cfx/prebuilt/common/bootanimation/720.zip:system/media/bootanimation.zip
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := xt897c
 PRODUCT_NAME := full_xt897c
