eb7cbe402d910dc2fd07a16241d141b95af047e4
arrrghhh <arrrghhh@xdandroid.com>
2013-07-24 09:29:23 -0600

    Fixed derps

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 add_lunch_combo aokp_xt897c-userdebug
 add_lunch_combo full_xt897c-userdebug
-=======
-add_lunch_combo cm_xt897c-userdebug
->>>>>>> d1e4090a7e82b5e05d271ba6c62723e469eb59f4
+
