705faf4600620d8d573a6aa66a17d9c6475b8216
arrrghhh <arrrghhh@xdandroid.com>
2013-11-14 21:50:12 -0700

    Fixup

---
+++ carbon.mk
@@ -0,0 +1,15 @@
+# Inherit some common Carbon
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 960
+
+# Release name
+PRODUCT_RELEASE_NAME := xt897c
+PRODUCT_NAME := carbon_xt897c
+
+$(call inherit-product, device/motorola/xt897c/full_xt897c.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_BRAND=motorola PRODUCT_NAME=xt897 BUILD_PRODUCT=xt897 BUILD_FINGERPRINT=motorola/XT897_us_spr/asanti_c:4.1.2/9.8.2Q-122_XT897_FFW-5/6:user/release-keys
+
